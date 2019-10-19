0ae03ca9d8af7e314e9cc0488873de22b974c243
juliemmasam <juliemmasam@gmail.com>
2019-10-19 14:54:21 +0300

    Translator Done 3

--- app/src/main/java/com/vinetworks/juliemmasam/alc_challenge/MainActivity.java
+++ app/src/main/java/com/vinetworks/juliemmasam/alc_challenge/MainActivity.java
@@ -1,14 +1,9 @@
+
 package com.vinetworks.juliemmasam.alc_challenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.animation.Animator;
-import android.animation.AnimatorInflater;
-import android.media.Image;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
-
 }
