8eaa4e79d2ea58d6c2c0cdf57b75aecacf72eab6
juliemmasam <juliemmasam@gmail.com>
2019-10-19 11:21:06 +0300

    Initial commit

---
+++ app/src/main/java/com/vinetworks/juliemmasam/alc_challenge/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vinetworks.juliemmasam.alc_challenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
